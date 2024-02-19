87e2a05de5c58d1c785ebd154a20c6879e86d4fc
Kate Molony <katemolony96@gmail.com>
2024-02-19 00:02:02 +0000

    create Controllers + Routes

--- src/controllers/dashboard-controller.js
+++ src/controllers/dashboard-controller.js
@@ -1,7 +1,24 @@
+import { db } from "../models/db.js";
+
 export const dashboardController = {
-    index: {
-      handler: async function (request, h) {
-        return h.view("main");
-      },
+  index: {
+    handler: async function (request, h) {
+      const locations = await db.locationStore.getAllLocations();
+      const viewData = {
+        title: "Placemark Dashboard",
+        locations: locations,
+      };
+      return h.view("dashboard-view", viewData);
     },
-  };
+  },
+
+  addLocation: {
+    handler: async function (request, h) {
+      const newLocation = {
+        title: request.payload.title,
+      };
+      await db.locationStore.addLocation(newLocation);
+      return h.redirect("/dashboard");
+    },
+  },
+};
