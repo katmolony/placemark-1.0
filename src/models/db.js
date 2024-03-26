a6b2a174862012b48e69ce9a0142baf713f9c583
Kate Molony <katemolony96@gmail.com>
2024-03-26 19:57:01 +0000

    Add models to db link

--- src/models/db.js
+++ src/models/db.js
@@ -1,6 +1,7 @@
 import { userMemStore } from "./mem/user-mem-store.js";
 import { locationMemStore } from "./mem/location-mem-store.js";
 import { businessMemStore } from "./mem/business-mem-store.js";
+import { reviewMemStore } from "./mem/review-mem-store.js";
 
 import { userJsonStore } from "./json/user-json-store.js";
 import { locationJsonStore } from "./json/location-json-store.js";
@@ -10,11 +11,13 @@ import { connectMongo } from "./mongo/connect.js";
 import { userMongoStore } from "./mongo/user-mongo-store.js";
 import { locationMongoStore } from "./mongo/location-mongo-store.js";
 import { businessMongoStore } from "./mongo/business-mongo-store.js";
+import { reviewMongoStore } from "./mongo/review-mongo-store.js";
 
 export const db = {
   userStore: null,
   locationStore: null,
   businessStore: null,
+  reviewStore: null,
 
   init(storeType) {
     switch (storeType) {
@@ -27,12 +30,14 @@ export const db = {
         this.userStore = userMongoStore;
         this.locationStore = locationMongoStore;
         this.businessStore = businessMongoStore;
+        this.reviewStore = reviewMongoStore;
         connectMongo();
         break;
       default:
         this.userStore = userMemStore;
         this.locationStore = locationMemStore;
         this.businessStore = businessMemStore;
+        this.reviewStore = reviewMemStore;
     }
   },
 };
